ae0638f61ae3205b2ca8be8e5906226a35f06292
Jonek Code <jonkujanek@gmail.com>
2018-08-12 22:02:15 +0100

    Minor changes

--- resources/views/components/projects/project.blade.php
+++ resources/views/components/projects/project.blade.php
@@ -10,6 +10,6 @@
         <i class="far fa-fw fa-star"></i><span>{{ $stars }}</span>
         <i class="fas fa-fw fa-code-branch"></i><span>{{ $forks }} </span>
         <i class="far fa-fw fa-eye"></i><span>{{ $views }}</span>
-        <span class="flexitem-right">0.01b</span>
+        <a class="flexitem-right" href="#"><i class="fas fa-file-download"></i><span>0.01b</span></a>
     </div>
 </div>
