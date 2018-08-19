3840d715c3a739891504a80d02139e7cacaa2466
Jonek Code <jonkujanek@gmail.com>
2018-08-19 02:45:05 +0100

    - Added contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts/default')
 @section('content')
-
+<div id="contact">
+    <form action="" method="post">
+        <div class="input-group">
+            <div class="input-group-label">
+                <span>Name: </span>
+            </div>
+            <input type="text" />
+        </div>
+        <div class="input-group">
+            <div class="input-group-label">
+                <span>Subject: </span>
+            </div>
+            <input type="text" />
+        </div>
+        <textarea rows="4" placeholder="Message..."></textarea>
+        <button type="submit">
+            <span>Send </span><i class="far fa-envelope"></i>
+        </button>
+    </form>
+</div>
 @endsection
