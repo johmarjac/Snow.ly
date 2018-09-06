dd57efed0d680ce149aeefaab4f0152b8c9b8cef
Jonek Code <jonkujanek@gmail.com>
2018-09-06 22:55:56 +0200

    Added og:props

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:site_name" content="{{ config('snowly.page_name') }}">
+        <meta property="og:title" content="{{ config('snowly.page_name') }} - @yield('title', '{Page}')">
+        <meta property="og:url" content="{{ Request::url() }}">
+        <meta property="og:description" content="">
         <link rel="stylesheet" type="text/css" href="/css/default.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         <script defer src="{{asset('js/app.js')}}"></script>
