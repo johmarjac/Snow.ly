24445c11193443c570d32ca8fa3849bae861baa4
Dominik Bauernfeind <d.bauernfeind@linemetrics.com>
2018-09-08 18:39:47 +0200

    REMOVE settings service provider ADD configuration file for default settings

---
+++ config/snowly.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    // Default Admin Account
+    'admin' => [
+        'name' => env('SNOWLY_ADMIN_NAME', 'admin'),
+        'password' => env('SNOWLY_ADMIN_PASS', 'admin'),
+    ],
+
+];
