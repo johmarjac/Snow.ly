82ed9d6292f7de0f70d6572604f90427e5cd89eb
JonekCode <30983493+jonekcode@users.noreply.github.com>
2018-09-27 14:03:28 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(!env('APP_KEY') == null)
+        if(env('APP_KEY') == null)
             return false;
         
         $settings = DB::table('settings')->get()->first();
